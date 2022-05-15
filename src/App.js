1ce6b25a31dea9a5c5c972705fed504d12aa5140
Kübra Tanrıkulu <ktanrikuluu@gmail.com>
2022-05-15 18:17:27 +0300

    create-react-route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
-import Navbar from './component/Navbar'
+import ReactDOM from "react-dom/client";
+import Navbar from './component/Navbar';
+import About from './pages/About';
+import Resume from './pages/Resume';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
     </div>
   );
 }
+export default function App1() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />} />
+          <Route path="pages/About" element={<About />} />
+          <Route path="pages/Resume" element={<Resume />} />
+          <Route path="pages/Blog" element={<Blog />} />
+          <Route path="pages/Contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App1 />);
 
-export default App;
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,43 +1,52 @@
 import React from 'react'
 import { BsGithub, BsMedium, BsInstagram, BsLinkedin } from "react-icons/bs";
 import { FaTwitterSquare } from "react-icons/fa";
+import { Outlet, Link } from "react-router-dom";
+
 
 const Navbar = () => {
     return (
-        <div style={{ backgroundImage: `url("https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720_960_720.jpg")`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+        <div className='home-page' style={{  backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition : 'center'}} >
             <div className='container'>
                 <nav class="navbar navbar-expand-lg navbar-light bg-transpatent">
                     <a class="navbar-brand d-none" href="#">Navbar</a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div style={{ fontSize: '20px' }} class="collapse navbar-collapse " id="navbarNav">
+                    <div style={{ fontSize: '23px' }} class="collapse navbar-collapse " id="navbarNav">
                         <ul class="navbar-nav ml-auto">
                             <li class="nav-item active">
-                                <a class="nav-link text-white" href="#about.html">About <span class="sr-only">(current)</span></a>
+                                <Link to="pages/About" class="nav-link text-white">About</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-white" href="#">Resume</a>
+                            <Link to="pages/Resume" class="nav-link text-white">Resume</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-white" href="#">Blog</a>
+                            <Link to="pages/Blog" class="nav-link text-white">Blog</Link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-white" href="#">Contact</a>
+                            <Link to="pages/Contact" class="nav-link text-white">Contact</Link>
                             </li>
                         </ul>
                     </div>
                 </nav>
+                <Outlet />
             </div>
-            <section style={{ marginTop: '15rem' }} className='p-2'>
+            <section style={{ marginTop: '15rem'}} className='p-2'>
+                <div className='container'>
                 <div className='row'>
+                    <div style={{ backgroundColor: 'rgba(0,0,0, 0.4)', border: '2px solid #f1f1f1'}} className='banner-text p-5 text-align-center'>
                     <h1 style={{ fontWeight: 'bolder' }} className='text-white display-1'>Kübra Tanrıkulu</h1>
-                    <h3 style={{ fontWeight: 'bold' }} className='text-white display-4 mt-5 p-3'>I'm Frontend Developer</h3></div>
+                    <h3 style={{ fontWeight: 'bold' }} className='text-white display-4 mt-5 p-3'>I'm Frontend Developer</h3>
+                    <h5 style={{ fontWeight: 'bold' }} className='text-white mt-5 p-3'>based in İstanbul</h5>
+                    </div>
+                    </div>
+                </div>
             </section>
             <section>
                 <div className='row'>
-                    <div className='col-12 d-flex justify-content-end'>
-                        <div style={{ fontSize: '25px' }} className='icons text-white p-5'>
+                    <div className='col-12 d-flex justify-content-lg-end flex-sm-row'>
+                        <div style={{ fontSize: '25px' }} className='col-12 icons text-white p-5 d-flex flex-lg-column'>
                             <div className='instagram'>
                                 <BsInstagram />
                             </div>
