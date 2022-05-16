928fb15c24bb211a2e66522c25cc596db343e1fc
Kübra Tanrıkulu <ktanrikuluu@gmail.com>
2022-05-16 20:41:05 +0300

    crearw-resume-page

---
+++ src/pages/Blog.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Blog() {
+  return (
+    <div>
+        <div className='container'>
+            <div className='row'>
+                <div className='blog-head text-center text-white'>
+                    <p>Check out my latest blog posts</p>
+                    <h1>My Blog</h1>
+                </div>
+                <div className='row'>
+                    <div className='col-12 col-md-4'>
+                        <img src=''/>
+                        <h3></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Blog
